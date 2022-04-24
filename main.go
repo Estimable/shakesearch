d4dba8f3b52f3ad01abf819d6a10ad4900f78a41
Blaine Willhoft <blaine.willhoft@gmail.com>
2022-04-24 18:39:53 -0500

    Blaine Willhoft - Completed Submission

--- main.go
+++ main.go
@@ -4,12 +4,74 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"index/suffixarray"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
 )
+
+var playTitles = []string{
+	"ALL’S WELL THAT ENDS WELL",
+	"ANTONY AND CLEOPATRA",
+	"AS YOU LIKE IT",
+	"THE COMEDY OF ERRORS",
+	"THE TRAGEDY OF CORIOLANUS",
+	"CYMBELINE",
+	"THE TRAGEDY OF HAMLET, PRINCE OF DENMARK",
+	"THE FIRST PART OF KING HENRY THE FOURTH",
+	"THE SECOND PART OF KING HENRY THE FOURTH",
+	"THE LIFE OF KING HENRY V",
+	"THE FIRST PART OF HENRY THE SIXTH",
+	"THE SECOND PART OF KING HENRY THE SIXTH",
+	"THE THIRD PART OF KING HENRY THE SIXTH",
+	"KING HENRY THE EIGHTH",
+	"KING JOHN",
+	"THE TRAGEDY OF JULIUS CAESAR",
+	"THE TRAGEDY OF KING LEAR",
+	"LOVE’S LABOUR’S LOST",
+	"MACBETH",
+	"MEASURE FOR MEASURE",
+	"THE MERCHANT OF VENICE",
+	"THE MERRY WIVES OF WINDSOR",
+	"A MIDSUMMER NIGHT’S DREAM",
+	"MUCH ADO ABOUT NOTHING",
+	"OTHELLO, THE MOOR OF VENICE",
+	"PERICLES, PRINCE OF TYRE",
+	"KING RICHARD THE SECOND",
+	"KING RICHARD THE THIRD",
+	"THE TRAGEDY OF ROMEO AND JULIET",
+	"THE TAMING OF THE SHREW",
+	"THE TEMPEST",
+	"THE LIFE OF TIMON OF ATHENS",
+	"THE TRAGEDY OF TITUS ANDRONICUS",
+	"THE HISTORY OF TROILUS AND CRESSIDA",
+	"TWELFTH NIGHT: OR, WHAT YOU WILL",
+	"THE TWO GENTLEMEN OF VERONA",
+	"THE TWO NOBLE KINSMEN",
+	"THE WINTER’S TALE",
+	"A LOVER’S COMPLAINT",
+	"THE PASSIONATE PILGRIM",
+	"THE PHOENIX AND THE TURTLE",
+	"THE RAPE OF LUCRECE",
+	"VENUS AND ADONIS"}
+
+type Searcher struct {
+	CompleteWorks string
+	Works         []Work
+}
+
+type Work struct {
+	Title       string
+	Text        string
+}
+
+type WorkResult struct {
+	Index   int
+	Title   string
+	Matches []string
+}
 
 func main() {
 	searcher := Searcher{}
@@ -22,6 +84,7 @@ func main() {
 	http.Handle("/", fs)
 
 	http.HandleFunc("/search", handleSearch(searcher))
+	http.HandleFunc("/work", handleWork(searcher))
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -33,11 +96,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-type Searcher struct {
-	CompleteWorks string
-	SuffixArray   *suffixarray.Index
 }
 
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
@@ -62,21 +120,112 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 	}
 }
 
+func handleWork(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		work, ok := r.URL.Query()["work"]
+		if !ok || len(work[0]) < 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("missing work query in URL params"))
+			return
+		}
+		workIndex, queryError := strconv.Atoi(work[0])
+		if queryError != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("non-integral id provided"))
+			return
+		}
+		results := searcher.GetWork(workIndex)
+		buf := &bytes.Buffer{}
+		enc := json.NewEncoder(buf)
+		err := enc.Encode(results)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("encoding failure"))
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(buf.Bytes())
+	}
+}
+
+func (s *Searcher) GetWork(index int) Work {
+	return s.Works[index]
+}
+
 func (s *Searcher) Load(filename string) error {
 	dat, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return fmt.Errorf("Load: %w", err)
 	}
 	s.CompleteWorks = string(dat)
-	s.SuffixArray = suffixarray.New(dat)
+
+	s.loadSonnets()
+	s.loadPlays()
+
 	return nil
 }
 
-func (s *Searcher) Search(query string) []string {
-	idxs := s.SuffixArray.Lookup([]byte(query), -1)
-	results := []string{}
-	for _, idx := range idxs {
-		results = append(results, s.CompleteWorks[idx-250:idx+250])
+func (s *Searcher) Search(query string) []WorkResult {
+	results := []WorkResult{}
+
+	for workIndex, work := range s.Works {
+		result := WorkResult{}
+		result.Index = workIndex
+		result.Title = work.Title
+
+		for _, matchIndex := range regexp.MustCompile("(?i)" + query).FindAllStringIndex(work.Text, -1) {
+			result.Matches = append(result.Matches, s.getLine(matchIndex[0], work.Text))
+		}
+
+		if (len(result.Matches) > 0) {
+			results = append(results, result)
+		}
 	}
 	return results
+}
+
+func (s *Searcher) loadSonnets() {
+	sonnetsStart := regexp.MustCompile(`\nTHE SONNETS`).FindStringIndex(s.CompleteWorks)[1]
+	sonnetsEnd := regexp.MustCompile(`\nTHE END`).FindStringIndex(s.CompleteWorks)[0]
+	sonnets := regexp.MustCompile(`(?:[^0-9\r\n]+\r\n)+`).FindAllString(s.CompleteWorks[sonnetsStart:sonnetsEnd], -1)
+	for index, sonnet := range sonnets {
+		work := Work{}
+		work.Title = "Sonnet " + strconv.Itoa(index + 1)
+		work.Text = sonnet
+		s.Works = append(s.Works, work);
+	}
+}
+
+func (s *Searcher) loadPlays() {
+	playsStart := regexp.MustCompile(`\nTHE END`).FindStringIndex(s.CompleteWorks)[1]
+	playsEnd := regexp.MustCompile(`CONTENT NOTE`).FindStringIndex(s.CompleteWorks)[0]
+
+	for titleIndex, title := range playTitles {
+		start := playsStart + regexp.MustCompile(title).FindStringIndex(s.CompleteWorks[playsStart:playsEnd])[1]
+		var end int
+		if (titleIndex == len(playTitles) - 1) {
+			end = playsEnd
+		} else {
+			end = playsStart + regexp.MustCompile(playTitles[titleIndex + 1]).FindStringIndex(s.CompleteWorks[playsStart:playsEnd])[0]
+		}
+		text := s.CompleteWorks[start:end]
+
+		work := Work{}
+		work.Title = title
+		work.Text = text
+		s.Works = append(s.Works, work);
+	}
+}
+
+func (s *Searcher) getLine(index int, source string) string {
+	var start, end = index, index
+
+	for source[start] != '\n' && source[start] != '\r' {
+		start--
+	}
+	for source[end] != '\n' && source[end] != '\r' {
+		end++
+	}
+
+	return source[start:end]
 }
--- static/app.js
+++ static/app.js
@@ -1,3 +1,7 @@
+const WORK_ID_ATTR_NAME = "work-id"
+const MATCH_ID_ATTR_NAME = "match-id"
+const QUERY_ATTR_NAME = "query-text"
+
 const Controller = {
   search: (ev) => {
     ev.preventDefault();
@@ -5,19 +9,105 @@ const Controller = {
     const data = Object.fromEntries(new FormData(form));
     const response = fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.displaySearchResults(results, data.query);
       });
     });
   },
 
-  updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
-    for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
+  getWork: (ev) => {
+    ev.preventDefault();
+    let workId = ev.target.getAttribute(WORK_ID_ATTR_NAME);
+    let matchId = ev.target.getAttribute(MATCH_ID_ATTR_NAME);
+    if (workId && matchId) {
+      const response = fetch(`/work?work=${workId}`).then((response) => {
+        response.json().then((work) => {
+          let query = document.getElementById("table-body").getAttribute(QUERY_ATTR_NAME);
+          Controller.displayWork(work, query, matchId);
+        });
+      });
+    } else {
+      console.error("no work id or match id found");
     }
-    table.innerHTML = rows;
   },
+
+  displaySearchResults: (results, query) => {
+    const table = document.getElementById("table-body");
+    table.setAttribute(QUERY_ATTR_NAME, query);
+    if (!results || results.length == 0) {
+      table.innerHTML = "No results found";
+    } else {
+      table.innerHTML = "";
+      let rows = [];
+      for (let result of results) {
+        let row = document.createElement("tr");
+        
+        let title = document.createElement("h3");
+        row.appendChild(title);
+        title.appendChild(document.createTextNode(result.Title));
+        
+        let matchList = document.createElement("ul");
+        row.appendChild(matchList);
+        result.Matches.forEach((match, matchIndex) => {
+          let matchItem = document.createElement("li");
+          matchItem.setAttribute(WORK_ID_ATTR_NAME, result.Index);
+          matchItem.setAttribute(MATCH_ID_ATTR_NAME, matchIndex);
+          matchItem.appendChild(document.createTextNode(match));
+          matchItem.onclick = Controller.getWork;
+          matchItem.style.color = 'blue';
+          matchItem.style.textDecoration = 'underline';
+          matchItem.style.cursor = 'pointer';
+          matchList.appendChild(matchItem);
+        });
+        
+        rows.push(row);
+      }
+      for (let row of rows) {
+        table.appendChild(row);
+      }
+    }
+  },
+
+  displayWork: (work, query, matchId) => {
+    const table = document.getElementById("table-body");
+    if (!work) {
+      table.innerHTML = "Work not found";
+    } else {
+      table.innerHTML = "";
+      let row = document.createElement("tr");
+      table.appendChild(row);
+
+      let title = document.createElement("h2");
+      row.appendChild(title);
+      title.appendChild(document.createTextNode(work.Title));
+    
+      let text = document.createElement("p");
+      row.appendChild(text);
+      text.innerHTML = work.Text.replace(/\r/g, '').replace(/\n/g, '<br>');
+
+      let matchNumber = 0;
+      let found = false;
+      let index = 0;
+      let lastElement;
+      let regex = new RegExp(query, 'i');
+      while (!found) {
+        let node = text.childNodes[index];
+        if (node instanceof Element) {
+          lastElement = node;
+        }
+        let content = node.textContent || node.innerText;
+        if (content && regex.test(content)) {
+          if (matchNumber == matchId) {
+            found = true;
+            console.log("found! " + content);
+            lastElement.scrollIntoView();
+          } else {
+            matchNumber++;
+          }
+        }
+        index++;
+      }
+    }
+  }
 };
 
 const form = document.getElementById("form");
